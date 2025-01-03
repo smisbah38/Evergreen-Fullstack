5bc37bf34ab0fa9beeca3de6572fb0029df0e861
smisbah38 <syedmisbah38@gmail.com>
2025-01-03 18:28:38 +0600

    Scrollbar issue fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -70,14 +70,14 @@ const Navbar = () => {
           {token && (
             <div className="group-hover:block hidden absolute dropdown-menu pt-2 right-0">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                <p className="cursor-pointer hover:text-black">My Profile</p>
+                {/* <p className="cursor-pointer hover:text-black">My Profile</p> */}
                 <p
                   onClick={() => {
                     navigate("/orders");
                   }}
                   className="cursor-pointer hover:text-black"
                 >
-                  Orders
+                  My Orders
                 </p>
                 <p onClick={logout} className="cursor-pointer hover:text-black">
                   Logout
